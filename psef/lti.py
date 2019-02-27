eada78d7ae29118d307251ff2be5b1896e44a89f
Olmo Kramer <olmo.kramer@protonmail.com>
2019-02-27 15:46:31 +0100

    Disable SSL verification

--- psef/lti.py
+++ psef/lti.py
@@ -1077,7 +1077,28 @@ class OutcomeRequest:
             key=self.consumer_key, secret=self.consumer_secret
         )
 
-        client = oauth2.Client(consumer)
+        class MyClient(oauth2.Client):
+            def __init__(self, consumer, token=None, cache=None, timeout=None,
+                proxy_info=None):
+                if consumer is not None and not isinstance(consumer, Consumer):
+                    raise ValueError("Invalid consumer.")
+
+                if token is not None and not isinstance(token, Token):
+                    raise ValueError("Invalid token.")
+
+                self.consumer = consumer
+                self.token = token
+                self.method = SignatureMethod_HMAC_SHA1()
+
+                httplib2.Http.__init__(
+                    self,
+                    cache=cache,
+                    timeout=timeout,
+                    proxy_info=proxy_info,
+                    disable_ssl_certificate_validation=True,
+                )
+
+        client = MyClient(consumer)
 
         # monkeypatch the _normalize function to ensures that the
         # ``Authorization`` header is NOT lower cased
