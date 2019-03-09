83531486cfacca88cb06e25f2517b3cffdcc16a2
Thomas Schaper <libre-man@users.noreply.github.com>
2019-03-09 12:22:24 +0100

    Remove show password button (#825)

--- src/components/PasswordInput.vue
+++ src/components/PasswordInput.vue
@@ -5,35 +5,16 @@
         <slot name="prepend">
             <b-input-group-prepend is-text v-if="label">{{ label }}</b-input-group-prepend>
         </slot>
-        <input v-if="visible"
-               :tabindex="tabindex"
-               type="text"
-               v-model="password"
-               class="form-control"
-               :placeholder="placeholder"/>
-        <input v-else
-               :tabindex="tabindex"
+        <input :tabindex="tabindex"
                type="password"
                v-model="password"
                class="form-control"
                :placeholder="placeholder"/>
-        <b-input-group-append>
-            <b-button @click="visible = !visible"
-                      @mouseenter="isToggleHovered = true"
-                      @mouseleave="isToggleHovered = false"
-                      variant="primary">
-                <icon :name="icon"/>
-            </b-button>
-        </b-input-group-append>
     </b-input-group>
 </b-form-fieldset>
 </template>
 
 <script>
-import Icon from 'vue-awesome/components/Icon';
-import 'vue-awesome/icons/eye';
-import 'vue-awesome/icons/eye-slash';
-
 export default {
     name: 'password-input',
 
@@ -60,16 +41,8 @@ export default {
     data() {
         return {
             visible: false,
-            isToggleHovered: false,
             password: this.value,
         };
-    },
-
-    computed: {
-        icon() {
-            const visible = this.isToggleHovered ? !this.visible : this.visible;
-            return visible ? 'eye' : 'eye-slash';
-        },
     },
 
     watch: {
@@ -80,10 +53,6 @@ export default {
         password() {
             this.$emit('input', this.password);
         },
-    },
-
-    components: {
-        Icon,
     },
 };
 </script>
