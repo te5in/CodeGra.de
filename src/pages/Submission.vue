157bf395141eb19535b5209380060570debcab81
Olmo Kramer <olmo.kramer+github@protonmail.com>
2019-03-12 01:38:45 +0100

    Fix file tree resizer z-index (#853)

--- src/pages/Submission.vue
+++ src/pages/Submission.vue
@@ -968,8 +968,9 @@ export default {
     .popover {
         max-width: 45em;
     }
-    .Resizer.columns {
-        padding: 0 15px;
+
+    .Resizer {
+        z-index: 0;
     }
 
     .Resizer.columns {
@@ -979,7 +980,9 @@ export default {
         width: 8px !important;
         transform: translateX(-4px);
         margin: 0;
+        padding: 0 15px;
         position: relative;
+
         &:before {
             display: block;
             content: '';
@@ -991,6 +994,7 @@ export default {
             left: 50%;
             border-left: 2px dotted @color-primary;
             border-right: 2px dotted @color-primary;
+
             #app.dark & {
                 border-color: @text-color-dark;
             }
