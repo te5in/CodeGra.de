63f9922c403b1ecbdf8ee407e08bd83b8be951a9
Thomas Schaper <thomas@libremail.nl>
2019-05-23 22:36:41 +0200

    Another (maybe futile) try to fix pyenv

--- seed_data/install_pyenv.sh
+++ seed_data/install_pyenv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "$HOME"
+env
 
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
      libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
@@ -8,4 +8,12 @@ sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
      git
 
 curl https://pyenv.run | bash
+
+echo -e '\neval "$(pyenv init -)"' | sudo tee --append /etc/profile
+echo -e '\neval "$(pyenv init -)"' >> /home/codegrade/.bash_profile
+echo -e '\neval "$(pyenv init -)"' >> /home/codegrade/.bashrc
 pyenv update
+
+bash -c 'env'
+bash -c 'which pyenv'
+bash -c 'echo $PATH | grep --color=auto "$(pyenv root)/shims"'
