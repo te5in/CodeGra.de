9fe04a8f3087947cb9cbb21f0ddf9b6db4bf23fb
Thomas Schaper <thomas@libremail.nl>
2019-05-23 20:38:21 +0200

    Improve pyenv setup script

--- seed_data/install_pyenv.sh
+++ seed_data/install_pyenv.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "$HOME"
+export PYENV_ROOT="/home/codegrade/"
+
 apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
 	libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
 	libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl \
