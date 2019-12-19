0df2ea6f9797c264dcf788c0f080ae01da189457
libre-man <libre-man@users.noreply.github.com>
2019-12-19 17:46:18 +0000

    Deploying to gh-pages from  e79384d018fb8f2341acdd44d25ed08968050866

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v1.16.12',
+    VERSION: 'v1.16.13',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
