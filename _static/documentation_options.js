86bf4388ca3ca0a76d819b4135a6467bea2fceca
olmokramer <olmo.kramer+github@protonmail.com>
2020-02-04 13:22:53 +0000

    Deploying to gh-pages from  ecb521e7cc6d1cb50ab29bd89375f12be6c705b4

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v1.18.4',
+    VERSION: 'v1.19.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
