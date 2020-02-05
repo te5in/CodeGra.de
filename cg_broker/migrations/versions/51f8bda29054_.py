26a0aa06bd6910f1d08de8b93a20414997b3b29e
Olmo Kramer <olmo.kramer@protonmail.com>
2020-02-05 13:41:14 +0100

    Merge migrations

---
+++ cg_broker/migrations/versions/51f8bda29054_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 51f8bda29054
+Revises: 0a408afab6df, ffdae601ed38
+Create Date: 2020-02-05 13:40:50.402802
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '51f8bda29054'
+down_revision = ('0a408afab6df', 'ffdae601ed38')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
