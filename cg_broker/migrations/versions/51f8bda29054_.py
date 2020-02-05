dc504ecd42bafe3b3fb83e275b0511daefc46449
Olmo Kramer <olmo.kramer@protonmail.com>
2020-02-05 13:56:37 +0100

    Fix formatting

--- cg_broker/migrations/versions/51f8bda29054_.py
+++ cg_broker/migrations/versions/51f8bda29054_.py
@@ -5,9 +5,8 @@ Revises: 0a408afab6df, ffdae601ed38
 Create Date: 2020-02-05 13:40:50.402802
 
 """
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = '51f8bda29054'
