b8d67ce3ade13e5cf010355c835346a5b61bf2f8
Olmo Kramer <olmo@codegra.de>
2020-03-23 17:24:28 +0100

    Rerender charts when dark mode setting is changed

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -158,6 +158,10 @@ export const BaseChart = {
         renderOptions() {
             this.rerender();
         },
+
+        darkMode() {
+            this.rerender();
+        },
     },
 
     mounted() {
