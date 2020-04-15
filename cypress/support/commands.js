435016844bc272cca4ae11d851950855a77ee672
Olmo Kramer <olmo@codegra.de>
2020-04-15 21:17:08 +0200

    Revert "Wait for input to be empty in cypress setText command"
    This reverts commit 577305bdd79387b47ad2fb7b58bdb2f21e345143.

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -536,7 +536,7 @@ Cypress.Commands.add('shouldNotOverflow', { prevSubject: true }, (subject) => {
 Cypress.Commands.add('setText', { prevSubject: true }, (subject, text) => {
     // Clear the input and type `text` if it is a nonempty string.
     const toWrite = `{selectall}{backspace}${text  || ''}`;
-    return cy.wrap(subject).clear().should('have.value', '').type(toWrite);
+    return cy.wrap(subject).clear().type(toWrite);
 });
 
 // Click a submit button, and optionally wait for its state to return back to
