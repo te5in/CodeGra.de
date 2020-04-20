985808c99c164b32a6d52dc5fca62b641c05a9e1
Thomas Schaper <thomas@codegra.de>
2020-04-20 12:42:07 +0200

    Remove on purpose error to test sentry

--- src/components/FeedbackArea.vue
+++ src/components/FeedbackArea.vue
@@ -1,7 +1,6 @@
 <!-- SPDX-License-Identifier: AGPL-3.0-only -->
 <template>
 <div class="feedback-area">
-    {{ (5 + '5').d.e }}
     <div class="replies-wrapper"
          :style="{ 'pb-2': showReply }">
         <transition-group name="fade"
