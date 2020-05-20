e0c2bda12dbf9c63a12665513347b4934da04eda
Olmo Kramer <olmo@codegrade.com>
2020-05-20 12:35:51 +0200

    Fix sidebar z-index to be less than modals

--- src/components/Sidebar/index.vue
+++ src/components/Sidebar/index.vue
@@ -620,8 +620,8 @@ export default {
 .sidebar {
     position: fixed;
     position: sticky;
-    // Must be greater than .sticky-top and .local-header
-    z-index: 1050;
+    // Must be greater than .sticky-top, .local-header, and modals
+    z-index: 1035;
     top: 0;
     height: 100vh;
 
