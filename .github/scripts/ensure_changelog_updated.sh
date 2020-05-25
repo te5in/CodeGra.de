cd9c34b62b874392f5f40723588d1b4f410aa59d
Olmo Kramer <olmo@codegrade.com>
2020-05-25 17:31:57 +0200

    Fix script?

--- .github/scripts/ensure_changelog_updated.sh
+++ .github/scripts/ensure_changelog_updated.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 set -o xtrace
 
-git diff --name-only master.. | grep '^docs/about/changelog.rst'
+if [ z "$(git diff master -- docs/about/changelog.rst | wc -l)" -eq 0 ]; then
+    printf 'Changelog was not updated!' >&2
+    exit 1
+fi
