0b84b43ce0c3874d779257879a56f429c2855ac5
Thomas Schaper <thomas@codegra.de>
2020-05-27 14:06:29 +0200

    Add docs for `psef/lti/v1_3/claims.py`

--- psef/lti/v1_3/claims.py
+++ psef/lti/v1_3/claims.py
@@ -1,3 +1,7 @@
+"""This module contains constants for the claims used in LTI 1.3 messages.
+
+SPDX-License-Identifier: AGPL-3.0-only
+"""
 from typing_extensions import Final
 
 CONTEXT: Final = 'https://purl.imsglobal.org/spec/lti/claim/context'
@@ -8,4 +12,6 @@ NAMESROLES: Final = (
     'https://purl.imsglobal.org/spec/lti-nrps/claim/namesroleservice'
 )
 GRADES: Final = 'https://purl.imsglobal.org/spec/lti-ags/claim/endpoint'
-DEEP_LINK: Final = 'https://purl.imsglobal.org/spec/lti-dl/claim/deep_linking_settings'
+DEEP_LINK: Final = (
+    'https://purl.imsglobal.org/spec/lti-dl/claim/deep_linking_settings'
+)
