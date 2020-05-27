de610914c38a9a25cbc73a2737fd3f898ae061fe
Thomas Schaper <thomas@codegra.de>
2020-05-27 14:03:24 +0200

    Add docs to `psef/lti/__init__.py`

--- psef/lti/__init__.py
+++ psef/lti/__init__.py
@@ -1,3 +1,10 @@
+"""This module implements the handling of LTI messages.
+
+It contains two submodules for the supported LTI version: :mod:`.v1_1` for LTI
+version 1.1 and :mod:`.v1_3` for LTI version 1.3.
+
+SPDX-License-Identifier: AGPL-3.0-only
+"""
 import enum
 import typing as t
 
@@ -7,6 +14,8 @@ __all__: t.List[str] = []
 
 
 class LTIVersion(enum.Enum):
+    """The supported LTI version.
+    """
     v1_1 = enum.auto()
     v1_3 = enum.auto()
 
@@ -15,6 +24,9 @@ class LTIVersion(enum.Enum):
 
 
 def init_app(app: PsefFlask) -> None:
-    from . import v1_1, v1_3
+    """Initialize LTI for the given flask app.
+    """
+    # This import is here to break some cyclic imports.
+    from . import v1_1, v1_3  # pylint: disable=import-outside-toplevel
     v1_1.init_app(app)
     v1_3.init_app(app)
