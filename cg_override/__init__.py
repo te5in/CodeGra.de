570d2abfbeb2c368cb20bd8c59ac6b8b6e62ceb0
Thomas Schaper <thomas@codegra.de>
2020-05-28 12:28:47 +0200

    Add `cg_override.no_override` decorator
    This makes sense for when you want to make sure you are not overriding a method
    by accident.

--- cg_override/__init__.py
+++ cg_override/__init__.py
@@ -1,10 +1,13 @@
 """A module containing an override decorator.
 
+.. todo::
+
+    Currently it is not possible to specify which class you expect to be
+    overriding. It would be very useful to add this in the future.
+
 SPDX-License-Identifier: AGPL-3.0-only
 """
 import typing as t
-
-from typing_extensions import Final
 
 T = t.TypeVar('T', bound=t.Callable)
 
@@ -17,5 +20,18 @@ def override(fun: T) -> T:
 
     :param fun: The function that is an override of a base class, this argument
         is simply returned.
+    """
+    return fun
+
+
+def no_override(fun: T) -> T:
+    """Indicate that this method does **not** override a method from a base
+        class.
+
+    This decorator doesn't introduce any runtime checks, but a accompanying
+    mypy plugin does check it.
+
+    :param fun: The function that is **not** an override of a base class, this
+        argument is simply returned.
     """
     return fun
--- cg_override/mypy_plugin.py
+++ cg_override/mypy_plugin.py
@@ -1,8 +1,12 @@
+"""This module implements the mypy plugin needed for the override decorators.
+
+SPDX-License-Identifier: AGPL-3.0-only
+"""
 from functools import partial
 
 from mypy.nodes import TypeInfo
 from mypy.types import Type, CallableType
-from mypy.plugin import Plugin, FunctionContext, AttributeContext
+from mypy.plugin import Plugin, FunctionContext
 
 
 def _has_method(name: str, cls: TypeInfo) -> bool:
@@ -11,24 +15,33 @@ def _has_method(name: str, cls: TypeInfo) -> bool:
     return any(_has_method(name, sub.type) for sub in cls.bases)
 
 
-def override_callback(ctx: FunctionContext) -> Type:
+def override_callback(ctx: FunctionContext, no_override: bool) -> Type:
     args = ctx.arg_types[0]
     ret = ctx.default_return_type
     if len(args) != 1:
         return ret
 
     fun = args[0]
-    if not isinstance(fun, CallableType) or fun.arg_names[0] != 'self':
+    if not isinstance(fun, CallableType) or fun.arg_names[0] not in ('self', 'cls'):
         ctx.api.fail('@override should be applied to method', ctx.context)
         return ret
 
-    cls_type = fun.arg_types[0].type
+    if fun.arg_names[0] == 'self':
+        cls_type = fun.arg_types[0].type
+    else:
+        assert fun.arg_names[0] == 'cls'
+        cls_type = fun.arg_types[0].item.type
 
-    if not any(
-        _has_method(ret.definition.name, base.type) for base in cls_type.bases
-    ):
+    assert isinstance(cls_type, TypeInfo)
+
+    overrides = any(_has_method(ret.definition.name, base.type) for base in cls_type.bases)
+    if no_override and overrides:
+        msg = f'The method {fun.name} is defined in a base class of {cls_type.name}'
+        ctx.api.fail(msg, ctx.context)
+    elif not no_override and not overrides:
         msg = f'The method {fun.name} is not defined in any base class of {cls_type.name}'
         ctx.api.fail(msg, ctx.context)
+
     return ret
 
 
@@ -36,7 +49,9 @@ class CgOverridePlugin(Plugin):
     def get_function_hook(self, fullname: str):
         if fullname == 'cg_override.override':
             # We need to return a method that will be called later on by mypy
-            return override_callback
+            return partial(override_callback, no_override=False)
+        if fullname == 'cg_override.no_override':
+            return partial(override_callback, no_override=True)
 
 
 def plugin(version: str):
