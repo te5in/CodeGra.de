c4a92cc84e02ec689ef2cb480a159b76908ed357
Olmo Kramer <olmo@codegrade.com>
2020-07-21 17:41:30 +0200

    Disable HotReload on GitHub Actions

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -11,25 +11,30 @@ Object.keys(baseWebpackConfig.entry).forEach(function (name) {
   baseWebpackConfig.entry[name] = ['./build/dev-client'].concat(baseWebpackConfig.entry[name])
 })
 
+const plugins = [
+  new webpack.DefinePlugin({
+    'process.env': config.dev.env
+  }),
+  new webpack.NoEmitOnErrorsPlugin(),
+  // https://github.com/ampedandwired/html-webpack-plugin
+  new HtmlWebpackPlugin({
+    filename: 'index.html',
+    template: 'index.html',
+    inject: true
+  }),
+  new FriendlyErrorsPlugin()
+];
+
+if (process.env.NODE_ENV === 'integration_testing') {
+  // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
+  plugins.push(new webpack.HotModuleReplacementPlugin());
+}
+
 module.exports = merge(baseWebpackConfig, {
   module: {
     rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap })
   },
   // cheap-module-eval-source-map is faster for development
   devtool: '#eval',
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': config.dev.env
-    }),
-    // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin(),
-    // https://github.com/ampedandwired/html-webpack-plugin
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'index.html',
-      inject: true
-    }),
-    new FriendlyErrorsPlugin()
-  ]
+  plugins,
 })
