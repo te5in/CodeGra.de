0e0ed8d26a8bfe8566525ce63a1022c8ce175c6d
Thomas Schaper <thoams@codegra.de>
2020-08-12 11:44:18 +0200

    Add merge migration

---
+++ migrations/versions/e7015056e7e2_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: e7015056e7e2
+Revises: 865724dddab2, f12cece11f62
+Create Date: 2020-08-12 11:43:12.229789
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e7015056e7e2'
+down_revision = ('865724dddab2', 'f12cece11f62')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
