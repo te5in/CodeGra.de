0e578bff8169e4129d2320280e8ba8915c6000cd
Thomas Schaper <thomas@codegra.de>
2020-08-13 17:31:29 +0200

    Add badges to exam assignment in sidebar

--- src/components/Sidebar/AssignmentListItem.vue
+++ src/components/Sidebar/AssignmentListItem.vue
@@ -6,7 +6,12 @@
                  :class="{ selected: submissionsSelected || (small && selected) }"
                  :to="submissionsRoute(assignment)">
         <div class="assignment-wrapper">
-            <span :title="assignment.name" class="assignment flex-grow-1 text-truncate">{{ assignment.name }}</span>
+            <span :title="assignment.name" class="assignment text-truncate">{{ assignment.name }}</span>
+            <div class="flex-grow-1 text-small-uppercase mx-2 d-flex align-self-center">
+                <b-badge class="exam-badge" v-if="assignment.kind === 'exam'" :variant="examBadgeVariant">
+                    exam
+                </b-badge>
+            </div>
 
             <assignment-state :assignment="assignment"
                               :editable="false"
@@ -43,6 +48,8 @@
 import Icon from 'vue-awesome/components/Icon';
 import 'vue-awesome/icons/gear';
 
+import { mapGetters } from 'vuex';
+
 import AssignmentState from '../AssignmentState';
 
 export default {
@@ -70,6 +77,8 @@ export default {
     },
 
     computed: {
+        ...mapGetters('pref', ['darkMode']),
+
         selected() {
             return this.assignment.id === this.currentId;
         },
@@ -80,6 +89,14 @@ export default {
 
         manageSelected() {
             return this.selected && this.$route.name === 'manage_assignment';
+        },
+
+        examBadgeVariant() {
+            if (this.darkMode) {
+                // This primary will be overridden by our own css.
+                return this.selected ? 'primary' : 'light';
+            }
+            return this.selected ? 'light' : 'primary';
         },
     },
 
@@ -159,6 +176,13 @@ a {
 
     .assignment {
         line-height: 1.1;
+    }
+}
+
+@{dark-mode} {
+    .light-selected .exam-badge {
+        color: white;
+        background-color: @color-primary;
     }
 }
 </style>
